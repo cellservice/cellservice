4f12bbe0631ae939f7dd03d8f63f73aa79e0a6a1
Ashish Kumar Ram <ashish.ram88@gmail.com>
2016-04-11 18:20:20 +0200

    update different parameters for Event class

--- app/src/main/java/de/tu_berlin/snet/cellactivity/util/Event.java
+++ app/src/main/java/de/tu_berlin/snet/cellactivity/util/Event.java
@@ -1,13 +1,35 @@
 package de.tu_berlin.snet.cellactivity.util;
 
+import android.location.Location;
+
 /**
  * Created by ashish on 31.03.16.
  */
 public class Event {
     public String type;
     public long timestamp;
+    public Location netLocation;
+    public Location gpsLocation;
+    public Location hiddenApiLocation;
+    public  Integer byteRxCount;
+    public Integer byteTxCount;
+    public int isProcessed;
+    public CellInfo cellinfo;
     public Event(String type, long timestamp){
         this.type= type;
         this.timestamp = timestamp;
+    }
+    public Event(String type, long timestamp, Integer byteRxCount, Integer byteTxCount){
+        this.type= type;
+        this.timestamp = timestamp;
+        this.byteRxCount = byteRxCount;
+        this.byteTxCount = byteTxCount;
+    }
+    public Event(String type, long timestamp, Integer byteRxCount, Integer byteTxCount, CellInfo cellInfo){
+        this.type= type;
+        this.timestamp = timestamp;
+        this.byteRxCount = byteRxCount;
+        this.byteTxCount = byteTxCount;
+        this.cellinfo = cellInfo;
     }
 }
