1c7a609e3747c6323f432b92ff067d7806722ccd
Friedhelm Victor <friedhelm.victor@tu-berlin.de>
2016-04-17 01:55:25 +0200

    Add LocationFuture class as alternative to AsyncEventResponseGPS/LP

---
+++ app/src/main/java/de/tu_berlin/snet/cellactivity/LocationFuture.java
@@ -0,0 +1,89 @@
+package de.tu_berlin.snet.cellactivity;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by giraffe on 4/15/16.
+ */
+public class LocationFuture implements Callable<Location>, LocationListener {
+
+    private LocationManager locationManager;
+    private String provider;
+    // minimum time interval between location updates, in milliseconds
+    // 6 seconds is the fastest time recommended by google for better battery usage.
+    final long MINIMUM_INTERVAL_TIME = 6 * 1000;
+    final long FRESHNESS_TIME = 10 * 1000; //10 seconds
+    private Location location = null;
+    CountDownLatch freshLocationReady = new CountDownLatch(1);
+
+    /**
+     * @param provider name of a provider as defined in Android's LocationManager.
+     *                 Obtainable through LocationManager.NETWORK_PROVIDER
+     */
+    public LocationFuture(String provider) {
+        this.provider = provider;
+        locationManager = (LocationManager) CellService.get().getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(provider, MINIMUM_INTERVAL_TIME, 0, this);
+    }
+
+    @Override
+    public Location call() {
+
+        try {
+            Location lastKnownLocation = locationManager.getLastKnownLocation(provider);
+            Log.d("Async: background", "Last known location from " + provider + " was: " + lastKnownLocation.toString());
+
+            // If the location's age is sufficiently fresh, return it
+            if (System.currentTimeMillis() - lastKnownLocation.getTime() < FRESHNESS_TIME) {
+                Log.d("Async: Background", "Known location is fresh");
+                locationManager.removeUpdates(this);
+                return lastKnownLocation;
+            } else {
+                // wait for the done signal be triggered by the onLocationChanged method
+                // If it doesn't happen after 2 minutes, the original value (null) will be returned
+                freshLocationReady.await(2, TimeUnit.MINUTES);
+
+                Log.d("Async: Background", "Returning fresh location");
+                return location;
+            }
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(Location newLocation) {
+        Log.d("Async: LocationChanged", "new location from " + newLocation.getProvider() + " available: " + newLocation.toString());
+        if (newLocation.getProvider().equals(this.provider)) {
+            this.location = newLocation;
+            //deregister the listener here
+            locationManager.removeUpdates(this);
+            freshLocationReady.countDown();
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Async: Background", "onProviderDisabled");
+    }
+}
