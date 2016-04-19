b31902f9e1ee5ca8925af6ef5c422b43cae049e4
Friedhelm Victor <friedhelm.victor@tu-berlin.de>
2016-04-19 17:38:03 +0200

    Update LocationFuture with freshness 2 minutes, Looper and error fixes

--- app/src/main/java/de/tu_berlin/snet/cellactivity/LocationFuture.java
+++ app/src/main/java/de/tu_berlin/snet/cellactivity/LocationFuture.java
@@ -5,6 +5,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 
 import java.util.concurrent.Callable;
@@ -21,7 +22,7 @@ public class LocationFuture implements Callable<Location>, LocationListener {
     // minimum time interval between location updates, in milliseconds
     // 6 seconds is the fastest time recommended by google for better battery usage.
     final long MINIMUM_INTERVAL_TIME = 6 * 1000;
-    final long FRESHNESS_TIME = 10 * 1000; //10 seconds
+    final long FRESHNESS_TIME = 120 * 1000; // 2 minutes
     private Location location = null;
     CountDownLatch freshLocationReady = new CountDownLatch(1);
 
@@ -32,7 +33,10 @@ public class LocationFuture implements Callable<Location>, LocationListener {
     public LocationFuture(String provider) {
         this.provider = provider;
         locationManager = (LocationManager) CellService.get().getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(provider, MINIMUM_INTERVAL_TIME, 0, this);
+        /* Note: to avoid exception:
+         * Can't create Handler inside thread that has not called Looper.prepare()
+         * Use solution suggestion: http://stackoverflow.com/a/29691963 (Adding Looper.getMainLooper()) */
+        locationManager.requestLocationUpdates(provider, MINIMUM_INTERVAL_TIME, 0, this, Looper.getMainLooper());
     }
 
     @Override
@@ -44,18 +48,19 @@ public class LocationFuture implements Callable<Location>, LocationListener {
 
             // If the location's age is sufficiently fresh, return it
             if (System.currentTimeMillis() - lastKnownLocation.getTime() < FRESHNESS_TIME) {
-                Log.d("Async: Background", "Known location is fresh");
+                Log.d("Async: Background", "Known " + provider + " location is fresh");
                 locationManager.removeUpdates(this);
                 return lastKnownLocation;
             } else {
                 // wait for the done signal be triggered by the onLocationChanged method
                 // If it doesn't happen after 2 minutes, the original value (null) will be returned
                 freshLocationReady.await(2, TimeUnit.MINUTES);
-
-                Log.d("Async: Background", "Returning fresh location");
+                Log.d("Async: Background", "Returning fresh "+provider+" location");
+                locationManager.removeUpdates(this);
                 return location;
             }
         } catch (Exception e) {
+            locationManager.removeUpdates(this);
             return null;
         }
     }
