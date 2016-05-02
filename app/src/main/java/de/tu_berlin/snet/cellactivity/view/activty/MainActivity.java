68c36d1bb7c862d7ef710c411b1526c2fde1eae0
Markus Paeschke <markus.paeschke@campus.tu-berlin.de>
2016-05-02 18:03:02 +0200

    Refactored main activity and related layout file a bit.

--- app/src/main/java/de/tu_berlin/snet/cellactivity/view/activty/MainActivity.java
+++ app/src/main/java/de/tu_berlin/snet/cellactivity/view/activty/MainActivity.java
@@ -14,94 +14,60 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import de.tu_berlin.snet.cellactivity.CellService;
 import de.tu_berlin.snet.cellactivity.R;
 import de.tu_berlin.snet.cellactivity.view.fragment.TabFragment;
 
-public class MainActivity extends AppCompatActivity {
-    DrawerLayout mDrawerLayout;
-    NavigationView mDrawerNavigationView;
-    FragmentManager mFragmentManager;
-    FragmentTransaction mFragmentTransaction;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    @Bind(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
+    @Bind(R.id.drawer_navigation)
+    NavigationView drawerNavigationView;
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        ButterKnife.bind(this);
+
         setSupportActionBar(toolbar);
 
-        /**
-         *Setup the DrawerLayout and NavigationView
-         */
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container_view, new TabFragment()).commit();
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        mDrawerNavigationView = (NavigationView) findViewById(R.id.drawer_navigation) ;
+        drawerNavigationView.setNavigationItemSelectedListener(this);
 
-        /**
-         * Lets inflate the very first fragment
-         * Here , we are inflating the TabFragment as the first Fragment
-         */
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.app_name, R.string.app_name);
 
-        mFragmentManager = getSupportFragmentManager();
-        mFragmentTransaction = mFragmentManager.beginTransaction();
-        mFragmentTransaction.replace(R.id.containerView,new TabFragment()).commit();
-        /**
-         * Setup click events on the Navigation View Items.
-         */
+        drawerLayout.addDrawerListener(drawerToggle);
 
-        mDrawerNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-                mDrawerLayout.closeDrawers();
-                /* for each drawer item
-
-                if (menuItem.getItemId() == R.id.nav_item) {
-                    FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.containerView, new TimeLineFragment()).commit();
-
-                } */
-
-                return false;
-            }
-
-        });
-
-        /**
-         * Setup Drawer Toggle of the Toolbar
-         */
-
-        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout, toolbar,R.string.app_name,
-                R.string.app_name);
-
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-
-        mDrawerToggle.syncState();
-
-
+        drawerToggle.syncState();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_start_service) {
-            startCellService();
-            return true;
-        } else if (id == R.id.action_stop_service) {
-            stopCellService();
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_start_service:
+                startCellService();
+                break;
+            case R.id.action_stop_service:
+                stopCellService();
+                break;
         }
 
         return super.onOptionsItemSelected(item);
@@ -129,18 +95,30 @@ public class MainActivity extends AppCompatActivity {
     }
     */
 
-
-    // my own onclick methods
-    public void startCellService() {
+    private void startCellService() {
         Intent i = new Intent(this, CellService.class);
         // if you want to pass data to the service
         i.putExtra("key", "some value");
         startService(i);
     }
 
-    public void stopCellService() {
+    private void stopCellService() {
         Intent i = new Intent(this, CellService.class);
         stopService(i);
         // alternative: stopSelf();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        drawerLayout.closeDrawers();
+                /* for each drawer item
+
+                if (menuItem.getItemId() == R.id.nav_item) {
+                    FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.containerView, new TimeLineFragment()).commit();
+
+                } */
+
+        return false;
     }
 }
